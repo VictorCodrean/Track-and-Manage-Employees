ba5865501ff5eb051f2ff33e2e5e43b2914c0e8c
Victor <VictorCodrean@users.noreply.github.com>
2021-03-25 17:11:36 -0700

    added ON DELETE CASCADE to be able to remove departments with all others nested childs

--- db/schema.sql
+++ db/schema.sql
@@ -17,6 +17,7 @@ CREATE TABLE role (
     department_id INT NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (department_id) REFERENCES department(id)
+     ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
@@ -27,4 +28,5 @@ CREATE TABLE employee (
     manager_id INT,
     PRIMARY KEY (id),
     FOREIGN KEY (role_id) REFERENCES role(id)
+    ON DELETE CASCADE
 );
